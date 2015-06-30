d132e3d3fd793ccbc32ab4b7fc0e46156df646b2
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-06-29 17:06:49 -0700

    Train the word2vec tool using the review data set.

--- WordVectors.py
+++ WordVectors.py
@@ -9,6 +9,11 @@ from bs4 import BeautifulSoup
 import re
 import nltk.data
 from nltk.corpus import stopwords
+import logging
+from gensim.models import word2vec
+
+# Configure logging for word2vec output
+logging.basicConfig(format = '%(asctime)s : %(levelname)s : %(message)s', level = logging.INFO)
 
 # Read data from files
 train = pd.read_csv("labeledTrainData.tsv", header = 0, delimiter = "\t", quoting = 3)
@@ -71,3 +76,21 @@ for review in unlabeled_train["review"]:
   sentences += review_to_sentences(review, tokenizer)
 
 print "Total number of sentences: %d\n" % (len(sentences)) 
+
+# Set values for word2vec parameters
+num_features = 300 # Word vector dimensionality
+min_word_count = 40 # Minimum word count
+num_workers = 4 # Number of threads to run in parallel
+context = 10 # Context window size
+downsampling = 1e-3 # Downsample setting for frequent words
+
+# Initialize and train the word2vec model
+print "Training model..."
+model = word2vec.Word2Vec(sentences, workers = num_workers, size = num_features, min_count = min_word_count, window = context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling init_sims will make the model much more memory-efficient
+model.init_sims(replace=True)
+
+# Can save the model for later use. You can load it later using Word2Vec.load()
+model_name = "300features_40minwords_10context"
+model.save(model_name)
